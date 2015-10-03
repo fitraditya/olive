7fd1dc03d5bb0a0174536e7cb0ff16488bcdb079
Fitra Aditya <fitra@g.pl>
2015-10-03 20:24:19 +0700

    Modify route

--- client/index.js
+++ client/index.js
@@ -2,10 +2,28 @@ exports.register = function (server, options, next) {
   server.route([
     {
       method: 'GET',
+      path: '/',
+      config: {
+        handler: function(request, reply) {
+          reply.file('public/index.html')
+        }
+      }
+    },
+    {
+      method: 'GET',
+      path: '/s/{session?}',
+      config: {
+        handler: function(request, reply) {
+          reply.file('public/session.html')
+        }
+      }
+    },
+    {
+      method: 'GET',
       path: '/{param*}',
       handler: {
         directory: {
-          path: 'public',
+          path: 'public/assets',
           listing: false
         }
       }
