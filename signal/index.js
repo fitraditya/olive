43431b46bd8a55c7143ed13aecd21b2777268113
Fitra Aditya <fitra@g.pl>
2015-10-04 01:01:30 +0700

    Fix signal, and add invite event

--- signal/index.js
+++ signal/index.js
@@ -1,6 +1,7 @@
 var _ = require('lodash-node')
 
 var users = []
+var sessions = {}
 
 exports.register = function (server, options, next) {
   var io = require('socket.io')(server.select('signal').listener)
@@ -13,34 +14,50 @@ exports.register = function (server, options, next) {
         return
       }
 
-      if (_.findIndex(users, { session: data.session }) !== -1) {
-        socket.emit('register_error', { message: 'This session already exists.' })
-        return
-      }
-
       users.push({
         socket: socket.id,
         session: data.session
       })
 
+      if (data.isInitiator) {
+        sessions[data.session] = {
+          initiator: socket.id,
+          users: []
+        }
+      }
+      sessions[data.session].users.push(socket.id)
+
       socket.emit('registered', {
-        id: socket.id,
+        socket: socket.id,
         session: data.session,
-        users: _.pluck(users, 'name')
+        users: sessions[data.session].users
       })
 
-      console.log('- ' + data.session + ' registered')
+      console.log('- ' + socket.id + ' registered, session ' + data.session)
+      console.log(sessions[data.session])
+    })
+
+    socket.on('invite', function (data) {
+      var user = _.find(users, { socket: socket.id })
+      if (!user) { return }
+
+      var target = _.find(users, { socket: data.target })
+      if (!target) { return }
+
+      io.to(target.socket).emit('invited', {
+        id: socket.id
+      })
     })
 
     socket.on('message', function (data) {
       var user = _.find(users, { socket: socket.id })
       if (!user) { return }
 
-      var contact = _.find(users, { session: data.session })
-      if (!contact) { return }
+      var target = _.find(users, { socket: data.target })
+      if (!target) { return }
 
-      io.to(contact.socket).emit('message', {
-        from: user.session,
+      io.to(target.socket).emit('message', {
+        id: socket.id,
         message: data.message
       })
     })
@@ -50,8 +67,7 @@ exports.register = function (server, options, next) {
 
       if (index !== -1) {
         users.splice(index, 1)
-
-        console.log('- ' + users[index].session + ' disconnected.')
+        console.log('- ' + socket.id + ' disconnected.')
       }
     })
 
