4c5ba295d9a7e495b3e313669bd870727ba837d7
Fitra Aditya <fitra@g.pl>
2015-10-04 01:16:14 +0700

    Refactor: change signaling from eventsource to websocket

--- public/assets/js/signal.js
+++ public/assets/js/signal.js
@@ -3,10 +3,19 @@ var Signal = function (sessionId) {
     window.location = '../'
   }
 
-  var userId = generateId()
-  var channels = {}
+  var myId = null
+  var peers = {}
 
   var socket = io('//localhost:4567')
+
+  var listeners = {
+    'onpeer': null,
+    'onsessionfull': null
+  }
+
+  for (var name in listeners) {
+    Object.defineProperty(this, name, createEventListenerDescriptor(name, listeners))
+  }
 
   socket.on('connect', function () {
     console.log('Connected to signaling server.')
@@ -17,19 +26,104 @@ var Signal = function (sessionId) {
   })
 
   socket.emit('register', {
-    session: sessionId
+    session: sessionId,
+    isInitiator: sessionStorage.getItem('isInisiator') || false
   });
 
   socket.on('registered', function (data) {
-    console.log('Registered as ' + data.id + ', session ' + data.session + '.')
-    console.log('Available users ' + data.users + '.')
+    myId = data.socket
+    console.log('Registered as ' + data.socket + ', session ' + data.session + '.')
+    
+    data.users.forEach(function (user) {
+      if (user !== myId) {
+        var id = user
+        var peer = new Peer(id)
+        
+        peers[id] = peer
+        fireEvent({
+          type: 'peer',
+          id: id,
+          peer: peer
+        }, listeners)
+
+        socket.emit('invite', {
+          target: user
+        })
+      }
+    })
   })
 
-  if (sessionStorage.getItem('isInisiator')) {
+  socket.on('invited', function (data) {
+    var id = data.id
+    var peer = new Peer(id)
     
+    peers[id] = peer
+    fireEvent({
+      type: 'peer',
+      id: id,
+      peer: peer
+    }, listeners)
+  })
+
+  socket.on('message', function (data) {
+    var id = data.id
+    var message = data.message
+    
+    peerHandler(id, message)
+  })
+
+  function Peer (id) {
+    var listeners = {
+      'onmessage': null,
+      'ondisconnect': null
+    }
+
+    for (var name in listeners) {
+      Object.defineProperty(this, name, createEventListenerDescriptor(name, listeners))
+    }
+
+    this.id = id
+
+    this.didMessage = function (id, data) {
+      fireEvent({ 'type': 'message', 'id': id, 'data': data }, listeners)
+    }
+
+    this.didDisconnect = function () {
+      fireEvent({ 'type': 'disconnect', 'id': id }, listeners)
+    }
+
+    this.send = function (id, message) {
+      socket.emit('message', {
+        target: id,
+        message: message
+      })
+    }
   }
 
-  function generateId () {
-    return Math.random().toString(16).substr(2)
+  function peerHandler (id, message) {
+    console.log(id)
+    console.log(peers)
+    var peer = peers[id]
+    
+    if (peer) {
+      peer.didMessage(id, message)
+      console.log(message)
+    }
+  }
+
+  function createEventListenerDescriptor (name, listeners) {
+    return {
+      'get': function () { return listeners[name] },
+      'set': function (cb) { listeners[name] = cb instanceof Function ? cb : null },
+      'enumerable': true
+    }
+  }
+
+  function fireEvent (event, listeners) {
+    var listener = listeners['on' + event.type]
+
+    if (listener) {
+      listener(event)
+    }
   }
 }
